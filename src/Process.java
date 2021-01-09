3a692f2b8741da07a82a8c3056c6f214283cb511
Nikolaos Diamantis <diamniko@csd.auth.gr>
2021-01-09 21:27:58 +0200

    Υλοποίηση κλάσης Process. Η κλάση ProcessControlBlock έχει υλοποιηθεί από την προηγούμενη υποβολή.

--- src/Process.java
+++ src/Process.java
@@ -19,57 +19,49 @@ public class Process {
     public void run() {
         /* TODO: you need to add some code here
          * Hint: this should run every time a process starts running */
-        if (pcb.getStopTimes().isEmpty()) {
-           // pcb.setState(ProcessState.RUNNING, (int) getResponseTime());
-        }
-        else {
-            pcb.setState(ProcessState.RUNNING, (int) getWaitingTime());
-        }
+        pcb.setState(ProcessState.RUNNING, CPU.clock);  //Αλλαγή κατάστασης σε RUNNING
     }
     
     public void waitInBackground() {
         /* TODO: you need to add some code here
          * Hint: this should run every time a process stops running */
-        if (getTurnAroundTime() == burstTime) {
-
+        //Αφαιρούμε τον χρόνο άφιξης γιατί από εκείνο το σημείο ξεκινάει να μετράει ο χρόνος αναμονής
+        double runningTime = CPU.clock - getWaitingTime() - arrivalTime;
+        if (runningTime == burstTime) {
+            pcb.setState(ProcessState.TERMINATED, CPU.clock);
         }
-        if (pcb.getState() == ProcessState.NEW) {
-            pcb.setState(ProcessState.READY, arrivalTime);  //Το arrivalTime δεν θα αποθηκευτεί σε κάποιο ArrayList
+        else {
+            pcb.setState(ProcessState.READY, CPU.clock);
         }
-        else if (pcb.getState() == ProcessState.RUNNING) {
-            if (getTurnAroundTime() - getWaitingTime() == burstTime) {
-                pcb.setState(ProcessState.TERMINATED, );
-            }
-        }
-        pcb.setState(ProcessState.READY, burstTime);
     }
 
     public double getWaitingTime() {
         /* TODO: you need to add some code here
          * and change the return value */
-        int sum = 0;
-        for (int i=0; i<pcb.getStopTimes().size(); i++) {
-            sum += pcb.getStartTimes().get(i+1) - pcb.getStopTimes().get(i);
+        double waitingTime = getResponseTime();
+        //Προσθέτουμε τα διαστήματα αναμονής από το πρώτο stop μέχρι το τελευταίο start
+        for (int i=1; i<pcb.getStartTimes().size(); i++) {
+            waitingTime += pcb.getStartTimes().get(i) - pcb.getStopTimes().get(i-1);
         }
-        return sum;
+        //Αν η διεργασία συνεχίζει να βρίσκεται σε αναμονή τότε ο χρόνος αναμονής συνεχίζει να αυξάνεται
+        if (pcb.getState() == ProcessState.READY) {
+            waitingTime += CPU.clock - pcb.getStopTimes().get(pcb.getStopTimes().size()-1);
+        }
+        return waitingTime;
     }
     
     public double getResponseTime() {
         /* TODO: you need to add some code here
          * and change the return value */
-        if (pcb.getStartTimes().isEmpty()) {
-            return 0;
-        }
-        return pcb.getStartTimes().get(0) - arrivalTime;
+        return pcb.getStartTimes().get(0) - arrivalTime;    //Η διαφορά μεταξύ του χρόνου εκκίνισης και του χρόνου άφιξης
     }
     
     public double getTurnAroundTime() {
         /* TODO: you need to add some code here
          * and change the return value */
-        int sum = 0;
-        for (int i=0; i<pcb.getStopTimes().size(); i++) {
-            sum += pcb.getStopTimes().get(i) - pcb.getStartTimes().get(i);
+        if (pcb.getState() == ProcessState.TERMINATED) {    //Επιστρέφει τιμή μόνο αν η διεργασία έχει ολοκληρωθεί
+            return pcb.getStopTimes().get(pcb.getStopTimes().size()-1) - arrivalTime;
         }
-        return sum;
+        return 0;
     }
 }
