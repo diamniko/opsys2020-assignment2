2dd266d31314be17aa84bf1608bdd8851519785d
Nikolaos Diamantis <diamniko@csd.auth.gr>
2021-01-14 23:48:54 +0200

    Υλοποίηση κλάσης CPU.

--- src/CPU.java
+++ src/CPU.java
@@ -18,12 +18,18 @@ public class CPU {
     public void run() {
         /* TODO: you need to add some code here
          * Hint: you need to run tick() in a loop, until there is nothing else to do... */
-
+        //Προσθέτουμε τις διεργασίες στον αλγόριθμο
+        for (currentProcess=0; currentProcess<processes.length; currentProcess++) {
+            tick();
+        }
     }
     
     public void tick() {
         /* TODO: you need to add some code here
          * Hint: this method should run once for every CPU cycle */
-        
+        //Ελέγχουμε αν μπορούμε να φορτώσουμε την διεργασία
+        if (mmu.loadProcessIntoRAM(processes[currentProcess])) {
+            scheduler.addProcess(processes[currentProcess]);
+        }
     }
 }
