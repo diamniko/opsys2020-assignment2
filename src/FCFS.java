942674839b32515d59252e5cf1dfc1d47cf0df5b
Nikolaos Diamantis <diamniko@csd.auth.gr>
2021-01-14 15:46:19 +0200

    Υλοποίηση κλάσης FCFS.

--- src/FCFS.java
+++ src/FCFS.java
@@ -3,15 +3,25 @@ public class FCFS extends Scheduler {
 
     public FCFS() {
         /* TODO: you _may_ need to add some code here */
+        while (processes.size() > 0) {
+            Process currentProcess = getNextProcess();
+            //Μετά την εκτέλεση μιας διεργασίας την σταματάει και την αφαιρεί από τη λίστα
+            currentProcess.waitInBackground();
+            removeProcess(currentProcess);
+        }
     }
 
     public void addProcess(Process p) {
         /* TODO: you need to add some code here */
+        processes.add(p);   //Απλά προσθέτει τη διεργασία p στη λίστα
     }
     
     public Process getNextProcess() {
         /* TODO: you need to add some code here
          * and change the return value */
-        return null;
+        //Παίρνει την πρώτη διεργασία από τη λίστα και την εκτελεί
+        Process newProcess = processes.get(0);
+        newProcess.run();
+        return newProcess;
     }
 }
