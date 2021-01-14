4efe2f9d61e2bf2649caa618791e109c393f3e85
KiziridisK <kiziridk@csd.auth.gr>
2021-01-14 23:18:53 +0200

    Documentation added.

--- src/BestFit.java
+++ src/BestFit.java
@@ -1,12 +1,37 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ *  Η συγκεκριμένη κλάση υλοποιεί τον αλγόριθμος "BestFit" .
+ *  Περιέχει βοηθητικές  μεθόδους που τον υλοποιούν.
+ *  Περισσότερες λεπτομέρειες παρακάτω.
+ */
 public class BestFit extends MemoryAllocationAlgorithm {
-    
+
+    /**
+     * Κατασκευαστής της κλάσης . Χρησιμοποιεί τον κατασκευαστή
+     * της abstract  κλάσης  " MemoryAllocationAlgorithm "
+     *
+     * @param availableBlockSizes Ένας πίνακας ακεραίων που περιέχει τα
+     *        μεγέθη των block μνήμης που μπορούν να χρησιμοποιηθούν.
+     */
     public BestFit(int[] availableBlockSizes) {
         super(availableBlockSizes);
     }
 
+    /**
+     * Μέθοδος, η οποία βρίσκει τον block στο οποίο πρέπει να αντιστοιχηθεί η
+     * διεργασία που αναλύουμε χρησιμοποιώντας τον αλγόριθμος "BestFit" .
+     * Συγκεκριμένα , ξεκινάμε με τον πίνακα "blocks" , στον  οποίο αλλάζουμε τα στοιχεία του,
+     * για να είναι απ' το μικρότερο στο μεγαλύτερο με την μέθοδο sort. Στην συνέχεια, ξεκινάμε
+     * μια προσπέλαση  του πίνακα συγκρίνωντας τα περιεχόμενά του με το MemoryRequirements
+     * της διεργασίας . Αν βρεθεί στοιχείο που το ικανοποιεί , επιστρέφουμε το μέγεθός του
+     * . Αν όχι , επιστρέφουμε -1.
+     *
+     * @param p Η διεργασία
+     * @param blocks ο πίνακας "blocks" για τα δεδομένα .  Στις κλήσεις θα περνάμε τον πίνακα "availableBlockSizes"
+     * @return Επιστρέφουμε το μέγεθος του block μνήμης που ικανοποιεί τις προϋποθέσεις.
+     */
    private int findFit(Process p, int[] blocks){
        int sizeOfBlock = 0;
        Arrays.sort(blocks);
@@ -21,6 +46,16 @@ public class BestFit extends MemoryAllocationAlgorithm {
        return sizeOfBlock;
    }
 
+    /**
+     * Μέθοδος , η οποία βρίσκει ποιο στοιχείο του "availableBlockSizes" έχει το
+     * μέγεθος , που μας ικανοποιεί .
+     * Προσπελαύνουμε όλον τον πίνακα και όταν βρούμε ότι τα στοιχεία ταυτίζονται
+     * επιστρέφουμε την θέση που υποδεικνύεται απ'  την μεταβλητή i.
+     *
+     * @param array Ο πίνακας "availableBlockSizes"
+     * @param sizeOfBlock Το μέγεθος που μας ικανοποιεί.
+     * @return Επιστρέφεται το index της θέσης του στοιχείου .
+     */
    private int findIndex(int[] array, int sizeOfBlock){
 
         for(int i=0;i<array.length-1;i++){
@@ -30,6 +65,23 @@ public class BestFit extends MemoryAllocationAlgorithm {
         }
         return -1;
    }
+
+    /**
+     * Μέθοδος που υλοποιεί την διαδικασία της αντιστοίχησης .
+     * Πρωτίστως βρίσκουμε εάν υπάρχει block  μνήμης που ικανοποιεί τις απαιτήσεις της διεργασίας
+     * χρησιμοποιώντας την μέθοδο findFit που αναπτύχθηκε παραπάνω.
+     * Αν η μέθοδος βρει το επιθυμητό block  μνήμης αποθηκεύουμε το μέγεθός του στην μεταβλητή sizeOfBlock . Αν όχι,
+     * η μέθοδος επιστρέφει -1  , σταματάει την εκτέλεση και βγάζει μήνυμα.
+     * Παρακάτω πάει στο currentlyUsedMemorySlots και βλέπει ποιο MemorySlot έχει το block  μνήμης βρέθηκε
+     * πάνω κάνωντας την αφαίρεση getBlockEnd() - getBlockStart() και συγκρίνοντας το με την sizeOfBlock .
+     * Αν βρεθεί το ζητούμενο , τότε η μεταβλητή address παίρνει την τιμή getStart() του MemorySlot , η οποία είναι
+     * η τιμή διεύθυνσης απ' όπου και ξεκινάει και αλλάζουμε την τιμή της μεταβλητής fit σε true.
+     * Τέλος , αφαιρούμε το συγκεκριμένο block απ το availableBlockSizes.
+     * @param p Η διεργασία που αναλύουμε
+     * @param currentlyUsedMemorySlots ArrayList που περιέχει τα MemorySlots που χρησιμοποιούνται.
+     * @return Η διέυθυνση μνήμης που περιέχεται στην μεταβλητή address εφόσον η μέθοδος ήταν επιτυχής , αλλίως
+     *          -1 για να δείξουμε, ότι δεν πέτυχε.
+     */
     public int fitProcess(Process p, ArrayList<MemorySlot> currentlyUsedMemorySlots) {
         boolean fit = false;
         int address = -1;
--- src/FirstFit.java
+++ src/FirstFit.java
@@ -1,11 +1,35 @@
 import java.util.ArrayList;
 
+/**
+ *  Η συγκεκριμένη κλάση υλοποιεί τον αλγόριθμος "FirstFit" .
+ *  Περιέχει βοηθητικές  μεθόδους που τον υλοποιούν.
+ *  Περισσότερες λεπτομέρειες παρακάτω.
+ */
 public class FirstFit extends MemoryAllocationAlgorithm {
-    
+
+    /**
+     * Κατασκευαστής της κλάσης . Χρησιμοποιεί τον κατασκευαστή
+     * της abstract  κλάσης  " MemoryAllocationAlgorithm "
+     *
+     * @param availableBlockSizes Ένας πίνακας ακεραίων που περιέχει τα
+     *        μεγέθη των block μνήμης που μπορούν να χρησιμοποιηθούν.
+     */
     public FirstFit(int[] availableBlockSizes) {
         super(availableBlockSizes);
     }
 
+    /**
+     * Μέθοδος, η οποία βρίσκει τον block στο οποίο πρέπει να αντιστοιχηθεί η
+     * διεργασία που αναλύουμε χρησιμοποιώντας τον αλγόριθμος "FirstFit" .
+     * Συγκεκριμένα , ξεκινάμε με τον πίνακα "blocks" , στον  οποίο αντιγράφουμε τα στοιχεία
+     * του πίνακαα availableBlockSizes. Στην συνέχεια, ξεκινάμε μια προσπέλαση  του πίνακα συγκρίνωντας
+     * τα περιεχόμενά του με το MemoryRequirement της διεργασίας .
+     * Αν βρεθεί στοιχείο που το ικανοποιεί , επιστρέφουμε το μέγεθός του. Αν όχι , επιστρέφουμε -1.
+     * Το στοιχείο αυτό είναι το πρώτο, το οποίο θα καλύπτει το MemoryRequirement της διεργασίας , αγνοώντας
+     * το γεγονός, ότι παρακάτω μπορεί να υπάρχουν blocks , που θα ταίριαζαν "καλύτερα".
+     * @param p Η διεργασία
+     * @return Επιστρέφουμε το μέγεθος του block μνήμης που ικανοποιεί τις προϋποθέσεις.
+     */
     private int findFit(Process p){
         int[] blocks = availableBlockSizes;
         for(int i = 0; i< blocks.length-1; i++){
@@ -16,6 +40,16 @@ public class FirstFit extends MemoryAllocationAlgorithm {
         return -1;
     }
 
+    /**
+     * Μέθοδος , η οποία βρίσκει ποιο στοιχείο του "availableBlockSizes" έχει το
+     * μέγεθος , που μας ικανοποιεί .
+     * Προσπελαύνουμε όλον τον πίνακα και όταν βρούμε ότι τα στοιχεία ταυτίζονται
+     * επιστρέφουμε την θέση που υποδεικνύεται απ'  την μεταβλητή i.
+     *
+     * @param array Ο πίνακας "availableBlockSizes"
+     * @param sizeOfBlock Το μέγεθος που μας ικανοποιεί.
+     * @return Επιστρέφεται το index της θέσης του στοιχείου .
+     */
     private int findIndex(int[] array, int sizeOfBlock){
 
         for(int i=0;i<array.length-1;i++){
@@ -26,6 +60,22 @@ public class FirstFit extends MemoryAllocationAlgorithm {
         return -1;
     }
 
+    /**
+     * Μέθοδος που υλοποιεί την διαδικασία της αντιστοίχησης .
+     * Πρωτίστως βρίσκουμε εάν υπάρχει block  μνήμης που ικανοποιεί τις απαιτήσεις της διεργασίας
+     * χρησιμοποιώντας την μέθοδο findFit που αναπτύχθηκε παραπάνω.
+     * Αν η μέθοδος βρει το επιθυμητό block  μνήμης αποθηκεύουμε το μέγεθός του στην μεταβλητή sizeOfBlock . Αν όχι,
+     * η μέθοδος επιστρέφει -1  , σταματάει την εκτέλεση και βγάζει μήνυμα.
+     * Παρακάτω πάει στο currentlyUsedMemorySlots και βλέπει ποιο MemorySlot έχει το block  μνήμης βρέθηκε
+     * πάνω κάνωντας την αφαίρεση getBlockEnd() - getBlockStart() και συγκρίνοντας το με την sizeOfBlock .
+     * Αν βρεθεί το ζητούμενο , τότε η μεταβλητή address παίρνει την τιμή getStart() του MemorySlot , η οποία είναι
+     * η τιμή διεύθυνσης απ' όπου και ξεκινάει και αλλάζουμε την τιμή της μεταβλητής fit σε true.
+     * Τέλος , αφαιρούμε το συγκεκριμένο block απ το availableBlockSizes.
+     * @param p Η διεργασία που αναλύουμε
+     * @param currentlyUsedMemorySlots ArrayList που περιέχει τα MemorySlots που χρησιμοποιούνται.
+     * @return Η διέυθυνση μνήμης που περιέχεται στην μεταβλητή address εφόσον η μέθοδος ήταν επιτυχής , αλλίως
+     *          -1 για να δείξουμε, ότι δεν πέτυχε.
+     */
     public int fitProcess(Process p, ArrayList<MemorySlot> currentlyUsedMemorySlots) {
         boolean fit = false;
         int address = -1;
